85beddc304859467bd6de75a9ad053aa2357ad4d
Galen Sho Sato <galen.sato@gmail.com>
2023-02-22 17:03:23 -0500

    Completed Lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value = ""):
+        self._value = value
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+      if type(value) == str:
+          self._value = value
+      else:
+        print("The value must be a string.")
+    
+    value = property(get_value, set_value) 
+    
+    def is_sentence(self):
+      return self._value[-1] == '.'
+    
+    def is_question(self):
+      return self._value[-1] == '?'
+    
+    def is_exclamation(self):
+      return self._value[-1] == '!'
+    
+    def count_sentences(self):
+      new_value = self._value.replace("?", "." )
+      new_value = new_value.replace("!", ".")
+      new_value = new_value.split(".")
+      new_value = [i for i in new_value if i]
+      return len(new_value) 
+
+
+      
+      
+
+
+     
